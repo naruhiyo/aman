ffe8a9aaca039875425e643a9f0dd5f984f0abd2
hiyoko3 <sb.gm.0823@gmail.com>
2020-10-12 12:07:17 +0900

    manコマンドからオプションを取得する

--- main.go
+++ main.go
@@ -1,15 +1,13 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/aman/filter"
 	"github.com/aman/iocontrol"
 	"github.com/aman/modules"
 	"github.com/nsf/termbox-go"
 )
 
-func main() {	
+func main() {
 	err := termbox.Init()
 	if err != nil {
 		panic(err)
@@ -17,15 +15,14 @@ func main() {
 	defer termbox.Close()
 
 	// 引数取得
-	var args = modules.Parse()
+	var args []string = modules.Parse()
 
 	// コマンド実行
-	var commandResult = modules.GetOptions(args)
+	var commandResult string = modules.ExecMan(args)
 
 	manLists := modules.AnalyzeOutput(commandResult)
 	var inputs = ""
 
-	fmt.Println("")
 	iocontrol.RenderResult(manLists[:])
 	for {
 		if iocontrol.ReceiveKeys(&inputs) == -1 {
