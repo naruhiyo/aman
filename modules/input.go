9ba8302444ab9f4ff717fe4042bd4c4e7f285a41
hiyoko3 <sb.gm.0823@gmail.com>
2020-10-12 12:07:17 +0900

    manコマンドからオプションを取得する

--- modules/input.go
+++ modules/input.go
@@ -1,10 +1,10 @@
 package modules
 
 import (
+	"bytes"
 	"errors"
 	"flag"
 	"os/exec"
-	"regexp"
 	"strings"
 )
 
@@ -23,40 +23,67 @@ func Parse() []string {
 }
 
 /**
-* コマンドを受け取り、オプションを取得する
+* man コマンドを実行する
 * @params args 実行時引数
 **/
-func GetOptions(args []string) string {
-	var options = [3]string{"-help", "--help", "-H"}
-	var command string = strings.Join(args, " ")
+func ExecMan(args []string) string {
+	// man コマンドは空白区切のコマンドをハイフンで管理しているため、ハイフンつなぎに変更
+	const MAN string = "man"
+	var command string = strings.Join(args, "-")
 
-	// それぞれのオプションでコマンドを実行する
-	var results []string
-	var cmdErrors []error
-	for _, option := range options {
-		var out, err = exec.Command(command, string(option)).Output()
+	// manコマンドを実行する
+	var out, err = exec.Command(MAN, command).Output()
 
-		if err != nil {
-			cmdErrors = append(cmdErrors, err)
-		} else {
-			results = append(results, string(out))
-		}
-	}
-
-	if len(cmdErrors) == len(options) {
+	if err != nil {
 		panic(errors.New("Error: No results"))
 	}
 
-	return strings.Join(results, ",")
+	return string(out)
 }
 
 /**
 * コマンド実行結果からオプションを抽出する
+* @params output manコマンド実行結果
+* @return オプションテキストリスト
 **/
 func AnalyzeOutput(output string) []string {
 	// === 条件 ===
 	// ハイフンまたはダブルハイフンで始まる英単語
-	reg := regexp.MustCompile(`-?-[a-zA-Z0-9\-]+`)
+	var splitOutputs []string = strings.Split(output, "\n")
 
-	return reg.FindAllString(output, -1)
+	// オプションを見つけたかどうか
+	var isFinding bool = true
+
+	// buffer の方が string結合より効率が良い
+	var buffer bytes.Buffer // オプション説明のブロックを入れる変数
+	var results []string
+
+	for _, line := range splitOutputs {
+		// オプション判定条件
+		// 1. 半角3文字以上の空白後に`--` または `-` で始まるオプションであること
+		// 2. 前のオプションの説明ではないこと
+		if isFinding {
+			// オプションが来るまでスキップ
+			if !(strings.Contains(line, "   --") || strings.Contains(line, "   -")) {
+				continue
+			}
+
+			isFinding = false
+			buffer.WriteString(line)
+		} else {
+			// 改行だった場合次のオプションを探す
+			if len(line) == 0 {
+				results = append(results, buffer.String())
+				buffer.Reset()
+				isFinding = true
+				continue
+			}
+			// バッファに文字列追加
+			buffer.WriteString("\n" + line)
+		}
+	}
+
+	results = append(results, buffer.String())
+
+	return results
 }
