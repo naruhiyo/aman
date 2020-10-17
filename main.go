299920f0401e9ce9bb0fbb56bf49326ac3fdf89d
hiyoko3 <sb.gm.0823@gmail.com>
2020-10-17 15:47:50 +0900

    [WIP] Selected row func.

--- main.go
+++ main.go
@@ -7,6 +7,13 @@ import (
 	"github.com/nsf/termbox-go"
 )
 
+const (
+	ESCAPE     = -1
+	KEY        = 1
+	ARROW_UP   = 90
+	ARROW_DOWN = 91
+)
+
 func main() {
 	err := termbox.Init()
 	if err != nil {
@@ -20,20 +27,35 @@ func main() {
 	// コマンド実行
 	var commandResult string = modules.ExecMan(args)
 
-	manLists := modules.AnalyzeOutput(commandResult)
-	var inputs = ""
+	var manLists []string = modules.AnalyzeOutput(commandResult)
+	var inputs string = ""
+	var selectedPos int = -1
+	var result []string = manLists
 
-	iocontroller := iocontrol.NewIoController(manLists)
+	iocontroller := iocontrol.NewIoController(result)
 	iocontrol.RenderQuery(&inputs)
-	pageList := iocontroller.LocatePages(manLists)
-	iocontroller.RenderResult(manLists, pageList[:])
+	pageList := iocontroller.LocatePages(result)
+	iocontroller.RenderResult(selectedPos, result, pageList[:])
 	for {
-		if iocontroller.ReceiveKeys(&inputs) == -1 {
+		var keyStatus int = iocontrol.ReceiveKeys(&inputs)
+		iocontrol.RenderQuery(&inputs)
+
+		switch keyStatus {
+		case KEY:
+			// 毎回 man 結果に対して検索を行う
+			result = filter.IncrementalSearch(&inputs, manLists)
+		case ARROW_UP:
+			if selectedPos > 0 {
+				selectedPos--
+			}
+		case ARROW_DOWN:
+			if selectedPos < len(result)-1 {
+				selectedPos++
+			}
+		case ESCAPE:
 			return
 		}
-		iocontrol.RenderQuery(&inputs)
-		result := filter.IncrementalSearch(&inputs, manLists)
 		pageList = iocontroller.LocatePages(result)
-		iocontroller.RenderResult(result, pageList[:])
+		iocontroller.RenderResult(selectedPos, result, pageList[:])
 	}
 }
